6edaf49431e3a6e659c90106de0561893e88c3cc
Hauwa Haruna <harounummie@gmail.com>
2024-01-19 18:16:48 +0100

    local time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+let apiKey = "0e07d3f80c4414708ec095toac29b8a4";
 
 function getCity(event) {
   event.preventDefault();
@@ -8,3 +10,28 @@ function getCity(event) {
 
 let formElement = document.querySelector("#search-form");
 formElement.addEventListener("submit", getCity);
+
+function localTime(date) {
+  e.preventDefault();
+  let day = document.querySelector("#day")
+  day = date.getDay();
+  hour = date.getHours();
+  minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ]
+
+  let currentDay = days[day];
+
+
+  
+}
+
+localTime();
