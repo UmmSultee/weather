c8cf97904a6df82a1cb6f3fe2ce8b7954ebe82e3
Hauwa Haruna <harounummie@gmail.com>
2024-01-21 12:07:08 +0100

    api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-let apiKey = "0e07d3f80c4414708ec095toac29b8a4";
-
-function getCity(event) {
-  event.preventDefault();
+function refreshWeather(response) {
   let cityElement = document.querySelector("#present-city");
-  let searchInput = document.querySelector("#search-input");
-  cityElement.innerHTML = searchInput.value;
-}
+  let temperatureElement = document.querySelector(".temperature");
+  let temperature = response.data.temperature.current;
+  let icon = document.querySelector(".icon");
+  let unit = document.querySelector(".unit");
+  let humidity = document.querySelector("#humodity");
+  let wind = document.querySelector("#wind");
+  let description = document.querySelector("#description");
+  let localTimeElement = document.querySelector("#day");
+  let dateElement = new Date(response.data.time * 1000);
 
-let formElement = document.querySelector("#search-form");
-formElement.addEventListener("submit", getCity);
+  cityElement.innerHTML = response.data.city;
+  
+  temperatureElement.innerHTML = Math.round(temperature)
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}"?>`;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  wind.innerHTML = `${response.data.wind.speed}km/h`
+  description.innerHTML = response.data.condition.description;
+  localTimeElement.innerHTML = localTime(date);
+  }
+
+
 
 function localTime(date) {
   e.preventDefault();
@@ -27,10 +38,35 @@ function localTime(date) {
     "Friday",
     "Saturday"
   ]
+  if (hour < 10){
+    hour = `0${hour}`
+  }
+
+  if (minutes < 10){
+    minutes = `0${minutes}`
+  }
 
   let currentDay = days[day];
   return `${currentDay} ${hour}:${minutes}`;
   
 }
 
-localTime();
+function getCity(city) {
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiKey = "0e07d3f80c4414708ec095toac29b8a4";
+    axios.get(apiUrl).then(refreshWeather)
+    
+}
+
+function searchEvent(event) {
+    e.preventDefault();
+    let searchInput = document.querySelector("#search-input");
+    getCity(searchInput.value);
+
+    
+}
+
+let searchFormElement = document.querySelector("#search-form")
+searchFormElement.addEventListener("submit", searchEvent);
+
+getCity("Kano")
